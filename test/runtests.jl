e7343ca966b8c7b8813008e7be4cf4758a8f5e14
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-07-28 03:32:23 -0500

    Fix test for wrapping/mutating a vector

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using CUDA
 using DLPack
 using PyCall
 using Test
@@ -5,19 +6,27 @@ using Test
 
 @info "Installing JAX"
 #
-pip = pyimport("pip")
-flags = split(get(ENV, "PIPFLAGS", ""))
-packages = ["jax[cpu]"]
-pip.main(["install"; flags; packages])
+packages = "jax[cpu]"
+run(PyCall.python_cmd(`-m pip install $packages`))
 
 
+# Load JAX
 jax = pyimport("jax")
 np = pyimport("jax.numpy")
 dlpack = pyimport("jax.dlpack")
 
+
 @testset "DLPack.jl" begin
-    v = np.asarray([1.0, 2.0, 3.0]; dtype = np.float64)
-    jv = unsafe_wrap(Array, DLVector{Float64}(dlpack.to_dlpack(v)))
-    jv[0] .= 0  # mutate a jax's tensor
-    @test py"$np.all($v[:] == $np.asarray([0.0, 2.0, 3.0])"
+    v = np.asarray([1.0, 2.0, 3.0])
+
+    dlv = DLVector{Float32}(@pycall dlpack.to_dlpack(v)::PyObject)
+    if DLPack.device_type(dlv) == DLPack.kDLCPU
+        jv = unsafe_wrap(Array, dlv)
+        jv[1] = 0  # mutate a jax's tensor
+    elseif DLPack.device_type(dlv) == DLPack.kDLGPU
+        jv = unsafe_wrap(CuArray, dlv)
+        jv[1:1] .= 0  # mutate a jax's tensor
+    end
+
+    @test py"$np.all($v[:] == $np.asarray([0.0, 2.0, 3.0])).copy().item()"
 end
