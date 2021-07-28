1b4843883835c5dba5a7dc02609ae1a51c6b08ce
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-07-28 02:17:38 -0500

    Config CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using DLPack
+using PyCall
 using Test
+
+
+@info "Installing JAX"
+#
+pip = pyimport("pip")
+flags = split(get(ENV, "PIPFLAGS", ""))
+packages = ["jax[cpu]"]
+pip.main(["install"; flags; packages])
+
 
 @testset "DLPack.jl" begin
     # Write your tests here.
