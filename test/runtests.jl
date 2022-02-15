ccf28e41b6cb7d98c9106b6663567a756c491530
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2022-02-15 10:14:37 -0600

    Add tests for `wrap`

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,31 +24,33 @@ jax.config.update("jax_enable_x64", true)
     to_dlpack = o -> @pycall dlpack.to_dlpack(o)::PyObject
 
     v = np.asarray([1.0, 2.0, 3.0], dtype = np.float32)
+    jv = DLPack.wrap(v, to_dlpack)
     dlv = DLPack.DLManagedTensor(to_dlpack(v))
     opaque_tensor = dlv.dl_tensor
 
     @test v.ndim == 1 == opaque_tensor.ndim
-    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[Float32]
+    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[eltype(jv)]
 
     if DLPack.device_type(opaque_tensor) == DLPack.kDLCPU
-        # dlv[1] = 0  # mutate a jax's tensor
+        jv[1] = 0  # mutate a jax's tensor
     elseif DLPack.device_type(opaque_tensor) == DLPack.kDLCUDA
-        # dlv[1:1] .= 0  # mutate a jax's tensor
+        jv[1:1] .= 0  # mutate a jax's tensor
     end
 
-    # @test py"$np.all($v[:] == $np.asarray([0.0, 2.0, 3.0])).item()"
+    @test py"$np.all($v == $np.asarray([0.0, 2.0, 3.0])).item()"
 
     w = np.asarray([1 2; 3 4], dtype = np.int64)
+    jw = DLPack.wrap(w, to_dlpack)
     dlw = DLPack.DLManagedTensor(to_dlpack(w))
     opaque_tensor = dlw.dl_tensor
 
     @test w.ndim == 2 == opaque_tensor.ndim
-    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[Int64]
+    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[eltype(jw)]
 
     if DLPack.device_type(opaque_tensor) == DLPack.kDLCPU
-        # @test dlw[2, 1] == 3
+        @test jw[1, 2] == 3  # dimensions are reversed
     elseif DLPack.device_type(opaque_tensor) == DLPack.kDLCUDA
-        # @test all(view(dlw, 2, 1) .== 3)
+        @test all(view(dlw, 1, 2) .== 3)  # dimensions are reversed
     end
 
 end
@@ -57,19 +59,20 @@ end
 @testset "PythonCall" begin
 
     v = torch.ones((2, 4), dtype = torch.float64)
+    jv = DLPack.wrap(v, torch.to_dlpack)
     dlv = DLPack.DLManagedTensor(torch.to_dlpack(v))
     opaque_tensor = dlv.dl_tensor
 
     @test pyconvert(Int, v.ndim) == 2 == opaque_tensor.ndim
-    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[Float64]
+    @test opaque_tensor.dtype == DLPack.jltypes_to_dtypes()[eltype(jv)]
 
     if DLPack.device_type(opaque_tensor) == DLPack.kDLCPU
-        # dlv[2] = 0  # mutate a jax's tensor
+        jv[5] = 0  # mutate a jax's tensor
     elseif DLPack.device_type(opaque_tensor) == DLPack.kDLCUDA
-        # dlv[2:2] .= 0  # mutate a jax's tensor
+        jv[5:5] .= 0  # mutate a jax's tensor
     end
 
-    # ref = torch.tensor(((1, 1, 1, 1), (0, 1, 1, 1)), dtype = torch.float64)
-    # @test Bool(torch.all(v == ref).item())
+    ref = torch.tensor(((1, 1, 1, 1), (0, 1, 1, 1)), dtype = torch.float64)
+    @test Bool(torch.all(v == ref).item())
 
 end
